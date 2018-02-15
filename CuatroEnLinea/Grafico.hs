84f3a792c00f48695e5bd3e7b8443ce43b442f6f
mfarias93 <36275989+mfarias93@users.noreply.github.com>
2018-02-14 23:00:41 -0300

    Se corrigen las funciones por listas de comprension
    se corrigen las unciones de lineas por listas de comprension que realizan las lineas horizontales y verticales

--- CuatroEnLinea/Grafico.hs
+++ CuatroEnLinea/Grafico.hs
@@ -59,66 +59,23 @@ toFloat x = (fromIntegral(x))::Float
 
 
 tablero :: Params -> G.Picture
-tablero para = G.Pictures [titulo para,linea2,linea3,linea4,linea5,linea6,linea7,linea8,linea10,linea11,linea12,linea13,linea14,linea15,linea16,linea17,linea19,linea20]
+tablero para = G.Pictures [titulo para,numero1,numeros,lineasHorizontal,lineasVertical]
 
 
+lineasHorizontal::G.Picture
+lineasHorizontal= G.Pictures [G.Line[(-319, x),(220,x)] | x<-[178,118..(-182)]]
 
-linea2:: G.Picture
-linea2 = G.Line[(-319,178),(220,178)]
-
-linea3:: G.Picture
-linea3 = G.Line[(-319,118),(220,118)]
-
-linea4:: G.Picture
-linea4 = G.Line[(-319,58),(220,58)]
-
-linea5:: G.Picture
-linea5 = G.Line[(-319,-2),(220,-2)]
-
-linea6:: G.Picture
-linea6 = G.Line[(-319,-62),(220,-62)]
-
-linea7:: G.Picture
-linea7 = G.Line[(-319,-122),(220,-122)]
-
-linea8:: G.Picture
-linea8 = G.Line[(-319,-182),(220,-182)]
-
-linea10:: G.Picture
-linea10 = G.Line[(-319,178),(-319,-182)]
-
-linea11:: G.Picture
-linea11 = G.Line[(-242,178),(-242,-182)]
-
-linea12:: G.Picture
-linea12 = G.Line[(-165,178),(-165,-182)]
-
-linea13:: G.Picture
-linea13 = G.Line[(-88,178),(-88,-182)]
-
-linea14:: G.Picture
-linea14 = G.Line[(-11,178),(-11,-182)]
-
-linea15:: G.Picture
-linea15 = G.Line[(66,178),(66,-182)]
-
-linea16:: G.Picture
-linea16 = G.Line[(143,178),(143,-182)]
-
-linea17:: G.Picture
-linea17 = G.Line[(220,178),(220,-182)]
+lineasVertical::G.Picture
+lineasVertical= G.Pictures [G.Line[(x, 178),(x,-182)] | x<-[(-319),(-242)..(220)]]
 
 
-linea21:: G.Picture
-linea21 = G.Line[(-270,238),(-270,-182)]
-
-linea19:: G.Picture
-linea19 = Translate (-305) (-225) $ 
+numero1:: G.Picture
+numero1 = Translate (-305) (-225) $ 
          Scale 1 0.3 $ 
          G.Text "1"
 
-linea20::G.Picture
-linea20  = Translate (-398) (-225) $ 
+numeros::G.Picture
+numeros  = Translate (-398) (-225) $ 
          Scale 1 0.3 $ 
          G.Text "* 234567*" 
 
@@ -136,8 +93,6 @@ fichaX y x = Translate (-430 + (77 * (x))) ( 270 - (60 * y)) $
                 circleSolid 130
 
 
-
-
 fichaO :: Float -> Float  -> G.Picture
 fichaO y x = Translate (-430 + (77 * (x))) ( 270 - (60 * y)) $ 
                 Scale 0.2 0.2 $ 
@@ -148,3 +103,5 @@ fichasVacias::String->G.Picture
 fichasVacias x  = Translate (-378) (210) $ 
                 Scale 0.2 0.2 $ 
                 G.Text ("Cantidad de tiros restantes :  "++ x)
+
+
